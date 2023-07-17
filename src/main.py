a9cb8c4cba0d332abf1f9baf2bb6b73fd32bea56
litepupp <116616252+litepupp@users.noreply.github.com>
2023-07-17 02:02:39 -0500

    conform to chatbox

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     while True:
         try:
             msg = input()
-            client.send_message("/filter", msg)
+            client.send_message("/chatbox/input", (msg, True, False))
             print("\033[1A" + "\033[K", end="")
         except KeyboardInterrupt:
             break
